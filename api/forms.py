de914575525d5bdf43f604631c59cad4a975f456
de12161 <162108835+de12161@users.noreply.github.com>
2024-09-26 18:04:13 +0300

    Update forms.py

--- api/forms.py
+++ api/forms.py
@@ -4,7 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FileField
 from wtforms.fields.form import FormField
 from wtforms.fields.list import FieldList
-from wtforms.fields.simple import TextAreaField
+from wtforms.fields.simple import TextAreaField, MultipleFileField
 from wtforms.validators import DataRequired, Regexp
 
 
@@ -18,17 +18,36 @@ class StrVarEntryForm(FlaskForm):
     value = StringField('', validators=[DataRequired()])
 
 
+class ImgVarEntryForm(FlaskForm):
+    value = MultipleFileField('', validators=[DataRequired()])
+
+
+class HTMLVarEntryForm(FlaskForm):
+    value = TextAreaField('', validators=[DataRequired()])
+
+
 class CustomForm(FlaskForm):
-    fields = FieldList(FormField(StrVarEntryForm))
+    text_fields = FieldList(FormField(StrVarEntryForm))
+    img_fields = FieldList(FormField(ImgVarEntryForm))
+    html_fields = FieldList(FormField(HTMLVarEntryForm))
     # doc_form = FileField('Template:', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
+"""
+    form_name and form_fields accept only valid variable names
+    (no numbers at starting position, only characters allowed are a-z, 0-9 and '_')
+    form_fields also checks for duplicates and has types (text, img and html) separated by ':'
+    example:
+    var:text
+    var2:html
+    var_img:img
+"""
 class AddFormForm(FlaskForm):
     form_name = StringField('Name', validators=[DataRequired(), Regexp(re.compile(r'^[a-z_]+[\w_]*(?!\s+)$', re.IGNORECASE))])
     form_fields = TextAreaField('Fields',
                            validators=[
                                DataRequired(),
-                               Regexp(re.compile(r'^(?!.*(\r?\n){2,}.*)[a-z_]+[\r\n\w_]*[\w_]+(?!.*(\r?\n))$', re.IGNORECASE | re.S))
+                               Regexp(re.compile(r'^(?!.*\s{2,}.*)(?!([\w_]+:\w+\r?\n)*([\w_]+):\w+\r?\n([\w_]+:\w+\r?\n)*\2:\w+(\s|$))([a-z_][\w_]*:(text|img|html)\r?\n)*[a-z_][\w_]*:(text|img|html)(?!\s+)$', re.IGNORECASE | re.S))
                            ])
     submit = SubmitField('Add form')
