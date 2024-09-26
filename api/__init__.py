45f5f29a40401e8777656e283b8fc568491bf8e0
de12161 <162108835+de12161@users.noreply.github.com>
2024-09-26 12:43:46 +0300

    Add custom names for custom form fields

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 
 from flask_wtf import CSRFProtect
-from werkzeug.utils import redirect
 
-from .forms import TestForm, AddFormForm, CustomForm
+from .forms import TestForm, AddFormForm, CustomForm, VarEntryForm
 
 from secrets import token_urlsafe
 
@@ -38,9 +37,14 @@ def add_form():
     form = AddFormForm()
     if form.validate_on_submit():
         form.fields.data = ''
+
         new_form = CustomForm()
+
         labels = request.form['fields'].split('\n')
         for label in labels:
-            new_form.fields.append_entry().label = label
+            new_form.fields.append_entry()
+            new_form.fields[-1].label = label.strip()
+            new_form.fields[-1].value.name = label.strip()
+
         form_list.append(new_form)
     return render_template('add_form.html', form=form)
