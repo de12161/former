b69b8a8529fdb2d8640671e9db9f194676994ed3
de12161 <162108835+de12161@users.noreply.github.com>
2024-09-29 16:15:33 +0300

    Update forms.py

--- api/forms.py
+++ api/forms.py
@@ -1,55 +1,31 @@
-from abc import ABC, abstractmethod
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.fields.choices import SelectField
-from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Regexp
 
 import re
 
 
-class FormFactoryBase(ABC):
-    def __init__(self, **kwargs):
-        self._form_kwargs = kwargs
+def create_form_class(**fields):
+    class CustomForm(FlaskForm): pass
 
-    def __getitem__(self, item):
-        return self._form_kwargs[item]
+    for field_name, field in fields.items():
+        setattr(CustomForm, field_name, field)
 
-    def __setitem__(self, key, value):
-        self._form_kwargs[key] = value
-
-    def __delitem__(self, key):
-        del self._form_kwargs[key]
-
-    @abstractmethod
-    def __call__(self):
-        pass
+    return CustomForm
 
 
-class CustomFormFactory(FormFactoryBase):
-    def __call__(self, **kwargs):
-        class CustomForm(FlaskForm):
-            pass
+def create_editor_class(**choices):
+    class EditorForm(FlaskForm):
+        field_name = StringField('Field name', validators=[
+            DataRequired(),
+            Regexp(re.compile(r'^(?!validator)[a-z]\w*(?!\s+)$', re.IGNORECASE))
+        ])
+        field_type = SelectField('Field type', validators=[DataRequired()], choices=choices)
+        add_field = SubmitField('Add field')
+        remove_field = SubmitField('Remove field')
 
-        for field_name, field in kwargs.items():
-            setattr(CustomForm, field_name, field)
-
-        return CustomForm(**self._form_kwargs)
-
-
-class EditorFormFactory(FormFactoryBase):
-    def __call__(self, **kwargs):
-        class EditorForm(FlaskForm):
-            field_name = StringField('Field name', validators=[
-                DataRequired(),
-                Regexp(re.compile(r'^(?!validator)[a-z]\w*(?!\s+)$', re.IGNORECASE))
-            ])
-            field_type = SelectField('Field type', validators=[DataRequired()], choices=kwargs)
-            add_field = SubmitField('Add field')
-            remove_field = SubmitField('Remove field')
-
-        return EditorForm(**self._form_kwargs)
+    return EditorForm
 
 
 class SaveFormForm(FlaskForm):
