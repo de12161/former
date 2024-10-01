c3d6f1a6bd22b2b37dfd81ceb51c80e702f513fa
de12161 <162108835+de12161@users.noreply.github.com>
2024-10-01 14:12:20 +0300

    Update db_utils.py

--- api/db_utils.py
+++ api/db_utils.py
@@ -1,30 +1,33 @@
 import sqlite3
-
-
-def connect(func):
-    def wrapper(self, *args, **kwargs):
-        with sqlite3.connect(self.db_name) as con:
-            ret = func(self, con, *args, **kwargs)
-        return ret
-    return wrapper
+from functools import wraps
 
 
 class FormDB:
-    def __init__(self, db_name):
-        self.__db_name = db_name
+    def __init__(self, db):
+        self.__con = sqlite3.connect(db)
 
-    @property
-    def db_name(self):
-        return self.__db_name
+    def __del__(self):
+        self.__con.commit()
+        self.__con.close()
 
-    @connect
-    def get_forms(self, _con):
-        cur = _con.cursor()
+    def __create_cursor(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            cur = self.__con.cursor()
+            ret = func(self, cur, *args, **kwargs)
+            cur.close()
+            return ret
+        return wrapper
 
+    @__create_cursor
+    def get_forms(self, _cur):
         forms = {}
 
-        rows = cur.execute('''
-        SELECT name_form, doc_form, name_field, type_field, name_choice FROM form LEFT JOIN field ON form.id_form = field.id_form LEFT JOIN choice ON field.id_field = choice.id_field
+        rows = _cur.execute('''
+        SELECT
+        name_form, doc_form, name_field, type_field, label_field, name_choice
+        FROM
+        form LEFT JOIN field ON form.id_form = field.id_form LEFT JOIN choice ON field.id_field = choice.id_field
         ''').fetchall()
 
         for row in rows:
@@ -32,7 +35,8 @@ class FormDB:
             doc = row[1]
             field = row[2]
             field_type = row[3]
-            choice = row[4]
+            field_label = row[4]
+            choice = row[5]
 
             if forms.get(form) is None:
                 forms[form] = {}
@@ -49,6 +53,7 @@ class FormDB:
                 forms[form]['fields'][field] = {}
 
             forms[form]['fields'][field]['type'] = field_type
+            forms[form]['fields'][field]['label'] = field_label
 
             if choice is None:
                 continue
@@ -60,35 +65,37 @@ class FormDB:
 
         return forms
 
-    @connect
-    def save_form(self, _con, form_name, fields, doc_form):
-        cur = _con.cursor()
-
-        cur.execute('INSERT INTO form(name_form, doc_form) VALUES (?, ?)', (form_name, doc_form))
+    @__create_cursor
+    def save_form(self, _cur, form_name, fields, doc_form):
+        _cur.execute('INSERT INTO form(name_form, doc_form) VALUES (?, ?)', (form_name, doc_form))
 
         if len(fields.items()) == 0:
             return
 
-        form_id = cur.execute('SELECT id_form FROM form WHERE name_form=?', (form_name,)).fetchone()[0]
+        form_id = _cur.execute('SELECT id_form FROM form WHERE name_form=?', (form_name,)).fetchone()[0]
 
         for field_name, field_data in fields.items():
-            cur.execute(
-                'INSERT INTO field(id_form, name_field, type_field) VALUES (?, ?, ?)',
-                (form_id, field_name, field_data['type'])
+            _cur.execute(
+                'INSERT INTO field(id_form, name_field, type_field, label_field) VALUES (?, ?, ?, ?)',
+                (form_id, field_name, field_data['type'], field_data['label'])
             )
 
             if field_data.get('choices') is None:
                 continue
 
-            field_id = cur.execute('SELECT id_field FROM field WHERE name_field=?', (field_name,)).fetchone()[0]
+            field_id = _cur.execute('SELECT id_field FROM field WHERE name_field=?', (field_name,)).fetchone()[0]
 
             for choice in field_data['choices']:
-                cur.execute('INSERT INTO choice(id_field, name_choice) VALUES (?, ?)', (field_id, choice))
+                _cur.execute('INSERT INTO choice(id_field, name_choice) VALUES (?, ?)', (field_id, choice))
 
 
 def init_db(db):
     with sqlite3.connect(db) as con:
         cur = con.cursor()
+
+        cur.execute('DROP TABLE IF EXISTS form')
+        cur.execute('DROP TABLE IF EXISTS field')
+        cur.execute('DROP TABLE IF EXISTS choice')
 
         cur.execute('''
         CREATE TABLE IF NOT EXISTS form (
@@ -97,16 +104,15 @@ def init_db(db):
         doc_form BLOB NOT NULL
         )
         ''')
-
         cur.execute('''
         CREATE TABLE IF NOT EXISTS field (
         id_field INTEGER PRIMARY KEY AUTOINCREMENT,
         id_form INTEGER REFERENCES form(id_form) ON DELETE CASCADE NOT NULL,
         name_field TEXT UNIQUE NOT NULL,
-        type_field INTEGER NOT NULL
+        type_field INTEGER NOT NULL,
+        label_field TEXT NOT NULL
         )
         ''')
-
         cur.execute('''
         CREATE TABLE IF NOT EXISTS choice (
         id_choice INTEGER PRIMARY KEY AUTOINCREMENT,
