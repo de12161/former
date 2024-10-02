8298a95525fbcdd5f8442700a339c9c97b7a614b
de12161 <162108835+de12161@users.noreply.github.com>
2024-10-02 16:23:02 +0300

    Remove unused import

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for, redirect, flash
+from flask import Flask, render_template, request, session, url_for, redirect
 from flask_bootstrap import Bootstrap5
 
 from flask_wtf import CSRFProtect
